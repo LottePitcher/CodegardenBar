77699788f0fbb0358c76f37c2366a4989f6c6b37
Lotte Pitcher <lotte@paminternet.co.uk>
2021-05-30 10:08:09 +0100

    test action to send tweet with countdown

--- CgBarBackend/TestController.cs
+++ CgBarBackend/TestController.cs
@@ -34,11 +34,55 @@ namespace CgBarBackend
             return new {user.Name, user.Location, user.Description};
         }
 
+        public async Task<object> TweetCountdown(string send = null)
+        {
+            var openingTimeUtc = new DateTime(2021, 06, 09, 18, 0, 0, DateTimeKind.Utc);
+            var timeUntilOpening = openingTimeUtc - DateTime.UtcNow;
+
+            var days = DisplayNumberWithUnit(timeUntilOpening.TotalDays, "day");
+            var hours = DisplayNumberWithUnit(timeUntilOpening.Hours, "hour");
+            var minutes = DisplayNumberWithUnit(timeUntilOpening.Minutes, "minute");
+
+            var displayTimeUntil = $"{days} {hours} and {minutes}";
+            if (timeUntilOpening.TotalDays == 0)
+            {
+                displayTimeUntil = $"{hours} and {minutes}";
+            }
+
+            string[] messages = 
+            {
+                $"If all goes to plan ... we'll be opening in {displayTimeUntil}!",
+                $"Not that we're worried or anything, but only {displayTimeUntil} until we open!",
+                $"In {displayTimeUntil} we should be able to open the doors!",
+                $"Someone call the brewery, there's only {displayTimeUntil} until we open!",
+            };
+
+            var rnd = new Random();
+            var offset = rnd.Next(0, 3);
+            var message = messages[offset];
+
+            if (send == "y")
+            {
+                var client = new TwitterClient(_twitterCredentialsSupplier.GetTwitterCredentials());
+                var tweet = await client.Tweets.PublishTweetAsync(messages[offset]);
+                return new { message = message, tweet_sent = true, tweetId = tweet.Id};
+            }
+
+            return new { message = message, tweet_sent = false };
+        }
+
         public async Task<IWebhookEnvironment[]> WebHookTests()
         {
             var environments = await _client.AccountActivity.GetAccountActivityWebhookEnvironmentsAsync().ConfigureAwait(false);
 
             return environments;
+        }
+
+        private string DisplayNumberWithUnit(double number, string singular, string plural = null)
+        {
+            if (plural == null) plural = singular + "s";
+            var displayNumber = Convert.ToInt32(number);
+            return (displayNumber == 1) ? displayNumber + " " + singular : displayNumber + " " + plural;
         }
     }
 
