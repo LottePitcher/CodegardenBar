ebf39da2f3f60f2c2cbad2a1c9bd29d035231e5b
Migaroez <geusens@gmail.com>
2021-06-08 10:15:10 +0200

    Added Bar controllers

--- CgBarBackend/Controllers/BarAdminController.cs
+++ CgBarBackend/Controllers/BarAdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CgBarBackend.Authorization;
+using CgBarBackend.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,9 +13,28 @@ namespace CgBarBackend.Controllers
     [Route("BarAdmin/[action]")]
     public class BarAdminController : ControllerBase
     {
+        private readonly IBarTender _barTender;
+
+        public BarAdminController(IBarTender barTender)
+        {
+            _barTender = barTender;
+        }
+
         public bool Ping()
         {
             return true;
+        }
+
+        [HttpPost]
+        public void AddPatron(string screenName, string name, string profileImage)
+        {
+            _barTender.AddPatron(screenName,name,profileImage);
+        }
+
+        [HttpPost]
+        public void OrderDrink(string screenName, string drink)
+        {
+            _barTender.OrderDrink(screenName,drink);
         }
     }
 }
---
+++ CgBarBackend/Controllers/BarController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CgBarBackend.Authorization;
+using CgBarBackend.Models;
+using CgBarBackend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CgBarBackend.Controllers
+{
+    [Route("Bar/[action]")]
+    public class BarController : ControllerBase
+    {
+        private readonly IBarTender _barTender;
+
+        public BarController(IBarTender barTender)
+        {
+            _barTender = barTender;
+        }
+
+        public bool Ping()
+        {
+            return true;
+        }
+
+        public IEnumerable<Patron> Patrons() => _barTender.Patrons;
+    }
+}
